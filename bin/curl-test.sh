d3658fb0583bec84fc3d50085975c9c272853ecb
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2014-12-18 23:24:41 -0500

    More initial files

---
+++ bin/curl-test.sh
@@ -0,0 +1,10 @@
+#/bin/bash -x
+
+# This didn't work with 'localhost' as the hostname. Had to switch to '127.0.0.1'.
+
+curl \
+	-v -v \
+	-X POST \
+	-H 'Content-Type: application/json' \
+	-d '{"username": "tommy", "body": "Fred lives in Peru with Zelda"}' \
+	http://127.0.0.1:3000/api/posts
