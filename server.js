833ca6b1f982472dbf2da0d26afa8145b0c71241
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2014-12-18 23:23:02 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+/**
+ * Created by tom on 12/18/14.
+ */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/api/posts', function (req, res, next) {
+    Post.find(function(err, posts) {
+        if (err) {
+            return next(err);
+        }
+        res.json(posts);
+    });
+});
+
+var Post = require('./models/post');
+app.post('/api/posts', function (req, res, next) {
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    });
+    post.save(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+        res.json(201, post);
+    })
+});
+
+app.listen(3000, function () {
+    console.log('Server listening on', 3000)
+});
